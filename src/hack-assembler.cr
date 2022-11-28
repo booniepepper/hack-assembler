20976f9d676aea8f1913d42b39d5cf83c5c77c3d
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-11-28 03:36:13 -0800

    Add some regexes

--- src/hack-assembler.cr
+++ src/hack-assembler.cr
@@ -1,5 +1,13 @@
 module Hack::Assembler
   VERSION = "0.1.0"
 
+
+  patterns = {
+    addr: /^@(.+)$/,
+    dest: /^([DAM][DAM]?[DAM]?)=/,
+    comp: /^(.+=)?(.+?)(;.+)?$/,
+    jmp: /^.+;(.+)$/
+  }
+
   puts "yo"
 end
