c3ff4be0f77fe93d9fa69310825b189a7a6e81a4
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-02 01:49:08 -0800

    Organize

--- src/hack-assembler.cr
+++ src/hack-assembler.cr
@@ -3,6 +3,8 @@ require "./util"
 
 stdin_lines do |line|
   instruction = parse_instruction line
-  binary = instruction_to_binary instruction
-  printf "%016b\n",  binary
+  unless instruction.nil?
+    binary = instruction_to_binary instruction
+     printf "%016b\n",  binary
+  end
 end
--- src/parse.cr
+++ src/parse.cr
@@ -2,8 +2,70 @@ alias AInstruction = NamedTuple(addr: UInt16)
 alias CInstruction = NamedTuple(dest: UInt16, comp: UInt16, jmp: UInt16)
 alias MalformedLine = NamedTuple(raw: String)
 
-def parse_instruction(raw : String) : AInstruction | CInstruction | MalformedLine
-  raw = raw.strip
+def parse_dest(str : String | Nil) : UInt16
+  {
+    nil => 0b000,
+    "M" => 0b001,
+    "D" => 0b010,
+    "MD" => 0b011,
+    "A" => 0b100,
+    "AM" => 0b101,
+    "AD" => 0b110,
+    "AMD" => 0b111,
+  }[str].to_u16
+end
+
+def parse_comp(str : String) : UInt16
+  {
+    "0" => 0b0101010,
+    "1" => 0b0111111,
+    "-1" => 0b0111010,
+    "D" => 0b0001100,
+    "A" => 0b0110000,
+    "M" => 0b1110000,
+    "!D" => 0b0001101,
+    "!A" => 0b0110001,
+    "!M" => 0b1110001,
+    "-D" => 0b0001111,
+    "-A" => 0b0110011,
+    "-M" => 0b1110011,
+    "D+1" => 0b0011111,
+    "A+1" => 0b0110111,
+    "M+1" => 0b1110111,
+    "D-1" => 0b0001110,
+    "A-1" => 0b0110010,
+    "M-1" => 0b1110010,
+    "D+A" => 0b0000010,
+    "D+M" => 0b1000010,
+    "D-A" => 0b0010011,
+    "D-M" => 0b1010011,
+    "A-D" => 0b0000111,
+    "M-D" => 0b1000111,
+    "D&A" => 0b0000000,
+    "D&M" => 0b1000000,
+    "D|A" => 0b0010101,
+    "D|M" => 0b1010101,
+  }[str].to_u16
+end
+
+def parse_jmp(str : String | Nil) : UInt16
+  {
+    nil => 0b000,
+    "JGT" => 0b001,
+    "JEQ" => 0b010,
+    "JGE" => 0b011,
+    "JLT" => 0b100,
+    "JNE" => 0b101,
+    "JLE" => 0b110,
+    "JMP" => 0b111,
+  }[str].to_u16
+end
+
+def parse_instruction(raw : String) : AInstruction | CInstruction | MalformedLine | Nil
+  raw = raw.gsub(/\/\/.+/, "").strip
+
+  return nil if raw == ""
+
   if raw.starts_with? '@'
     addr = /^@(.+)$/.match(raw).try {|match| UInt16.new match[1]}
 
@@ -11,68 +73,15 @@ def parse_instruction(raw : String) : AInstruction | CInstruction | MalformedLin
       return { addr: addr }
     end
   else
-    dest_s = /^([DAM][DAM]?[DAM]?)=/.match(raw).try(&.[1])
-    comp_s = /^(.+=)?(.+?)(;.+)?$/.match(raw).try(&.[2])
-    jmp_s = /^.+;(.+)$/.match(raw).try(&.[1])
+    dest = /^([DAM][DAM]?[DAM]?)=/.match(raw).try(&.[1])
+    comp = /^(.+=)?(.+?)(;.+)?$/.match(raw).try(&.[2])
+    jmp = /^.+;(.+)$/.match(raw).try(&.[1])
 
-    unless dest_s.nil? || comp_s.nil? || jmp_s.nil?
-      dest : UInt16 = {
-        "null" => 0b000,
-        "M" => 0b001,
-        "D" => 0b010,
-        "MD" => 0b011,
-        "A" => 0b100,
-        "AM" => 0b101,
-        "AD" => 0b110,
-        "AMD" => 0b111,
-      }[dest_s].to_u16
-
-      comp : UInt16 = {
-        "0" => 0b0101010,
-        "1" => 0b0111111,
-        "-1" => 0b0111010,
-        "D" => 0b0001100,
-        "A" => 0b0110000,
-        "M" => 0b1110000,
-        "!D" => 0b0001101,
-        "!A" => 0b0110001,
-        "!M" => 0b1110001,
-        "-D" => 0b0001111,
-        "-A" => 0b0110011,
-        "-M" => 0b1110011,
-        "D+1" => 0b0011111,
-        "A+1" => 0b0110111,
-        "M+1" => 0b1110111,
-        "D-1" => 0b0001110,
-        "A-1" => 0b0110010,
-        "M-1" => 0b1110010,
-        "D+A" => 0b0000010,
-        "D+M" => 0b1000010,
-        "D-A" => 0b0010011,
-        "D-M" => 0b1010011,
-        "A-D" => 0b0000111,
-        "M-D" => 0b1000111,
-        "D&A" => 0b0000000,
-        "D&M" => 0b1000000,
-        "D|A" => 0b0010101,
-        "D|M" => 0b1010101,
-      }[comp_s].to_u16
-
-      jmp : UInt16 = {
-        "null" => 0b000,
-        "JGT" => 0b001,
-        "JEQ" => 0b010,
-        "JGE" => 0b011,
-        "JLT" => 0b100,
-        "JNE" => 0b101,
-        "JLE" => 0b110,
-        "JMP" => 0b111,
-      }[jmp_s].to_u16
-    
+    unless comp.nil?
       return {
-        dest: dest,
-        comp: comp,
-        jmp: jmp,
+        dest: parse_dest(dest),
+        comp: parse_comp(comp),
+        jmp: parse_jmp(jmp),
       }
     end
   end
