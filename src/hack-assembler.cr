701cd00b485f684979677337cdb51c49c76aa810
ｈｉｌｊｕｓｔｉ <hiljusti@so.dang.cool>
2022-12-02 16:57:34 -0800

    Convert to a more common CLI invocation

--- src/hack-assembler.cr
+++ src/hack-assembler.cr
@@ -1,10 +1,60 @@
+require "option_parser"
 require "./parse"
 require "./util"
 
 # TODO: Take filename as input, -o flag for output (default, name with extension of .hack)
 # TODO: Symbols
 
-stdin_lines do |line|
+infile_input : String | Nil = nil
+outfile_input : String | Nil = nil
+
+option_parser = OptionParser.parse do |parser|
+  parser.banner = "Usage: hasm [-o OUTFILE] INFILE"
+
+  parser.on "-v", "--version", "Show version" do
+    puts "hasm 0.1"
+    exit
+  end
+
+  out_desc = "Specify output file (default is infile with .hack extension)"
+
+  parser.on "-o OUTFILE", "--outfile OUTFILE", out_desc do |filename|
+    outfile_input = filename
+  end
+
+  parser.on "-h", "--help", "Show help" do
+    puts parser
+    exit
+  end
+
+  parser.unknown_args do |args, _|
+    if args.size > 0
+      infile_input = args.first
+    end
+  end
+end
+
+if infile_input.nil?
+  STDERR.puts "Missing input file."
+  exit 1
+end
+
+infile : String = infile_input.not_nil!
+
+if ! File.readable? infile
+  STDERR.puts "Not a readable file: #{infile.inspect}"
+  exit 1
+end
+
+outfile : String
+
+if outfile_input.nil?
+  outfile = infile.gsub(/\.[^\.]+$/, "") + ".hack"
+else
+  outfile = outfile_input.not_nil!
+end
+
+File.each_line infile do |line|
   instruction = parse_instruction line
   unless instruction.nil?
     binary = instruction_to_binary instruction
